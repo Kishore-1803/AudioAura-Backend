41b96517b26b1f706a89d56d7f10a3c8ad09d7e7
Kishore Balaji <160571228+Kishore-1803@users.noreply.github.com>
2025-01-05 17:06:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/User', {
+mongoose.connect('mongodb://localhost:27017/User', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
